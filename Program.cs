eb39b0b7b2b1a6b1ef1b09e4db7ead994abe6d6f
Korchenyuk Nazar <s32908@pjwstk.edu.pl>
2026-03-10 13:39:00 +0100

    User input handling added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter numbers separated by space:");
+var input = Console.ReadLine();
