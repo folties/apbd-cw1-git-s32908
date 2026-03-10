8bff7748829fca2ccae1c998ccf7a58c44f514c0
Korchenyuk Nazar <s32908@pjwstk.edu.pl>
2026-03-10 13:39:54 +0100

    Input validation added

--- Program.cs
+++ Program.cs
@@ -3,3 +3,8 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Enter numbers separated by space:");
 var input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("No numbers entered");
+}
